e3023a5cbbfb0f1f43b590464b3003b73104759a
Uriyah Adriel Sam <uriyahadrielsam@gmail.com>
2022-09-21 14:23:25 -0700

    mod 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /*
- * main - Entry Point
+ * main: Entry Point
  * return: Always 0 (Success)
  */
-//main - Entry Point
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
